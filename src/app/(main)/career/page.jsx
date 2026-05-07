31d32e2fae265a5924624d40353b4a868ffbedbc
pathikroy86 <pathik.roy86@gmail.com>
2026-05-07 18:06:24 +0600

    demo card in career

--- src/app/(main)/career/page.jsx
+++ src/app/(main)/career/page.jsx
@@ -8,7 +8,18 @@ export const metadata = {
 const CareerPage = () => {
     return (
         <div>
-            This is career Page. come here
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
